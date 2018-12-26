f865e27d44b3ce2327e0d9a59af570e6adc65e59
FatbardhKadriu <44509862+FatbardhKadriu@users.noreply.github.com>
2018-12-26 08:56:22 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,11 +2,16 @@ function checkforblank(){
 			if(document.getElementById('EmriMbiemri').value == ""){
 				alert('Ju lutem jepni Emri dhe Mbiemrin tuaj');
 				document.getElementById('EmriMbiemri').style.borderColor = "red";
+				document.getElementById('EmriMbiemri').style.color = "white";
+				document.getElementById('EmriMbiemri').style.background = "red";
+				
 				return false;
 			}
 			else if(document.getElementById('email').value == ""){
 				alert('Ju lutem jepni emailin tuaj');
 				document.getElementById('email').style.borderColor = "red";
+				document.getElementById('email').style.color = "white";
+				document.getElementById('email').style.background = "red";
 				return false;
 			}
 		}
