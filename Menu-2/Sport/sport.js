12d734c4f31fde19dc03832753d9183d59f8f932
FatbardhKadriu <44509862+FatbardhKadriu@users.noreply.github.com>
2019-01-10 13:37:31 +0100

    .

---
+++ Menu-2/Sport/sport.js
@@ -0,0 +1,25 @@
+function openKyqu(){
+			window.open("../../Menu-1/kyqu.html","_blank","top=40,bottom=400,left=600,height=600,width=600");
+		}
+		function openRegjistrohu(){
+			window.open("../../Menu-1/signup.html","_blank","top=0,left=200,height=700,width=1000");
+		}
+
+		function checkforblank(){
+			if(document.getElementById('EmriMbiemri').value == ""){
+				alert('Ju lutem jepni Emri dhe Mbiemrin tuaj');
+				document.getElementById('EmriMbiemri').style.borderColor = "red";
+				document.getElementById('EmriMbiemri').style.color = "white";
+				document.getElementById('EmriMbiemri').style.background = "red";
+				
+				return false;
+			}
+			else if(document.getElementById('email').value == ""){
+				alert('Ju lutem jepni emailin tuaj');
+				document.getElementById('email').style.borderColor = "red";
+				document.getElementById('email').style.color = "white";
+				document.getElementById('email').style.background = "red";
+				return false;
+			}
+
+		}
--- Menu-2/Teknologji/teknologji.js
+++ Menu-2/Teknologji/teknologji.js
@@ -8,27 +8,19 @@ function checkforblank(){
 				
 				return false;
 			}
-			 if(document.getElementById('email').value == ""){
+			else if(document.getElementById('email').value == ""){
 				alert('Ju lutem jepni emailin tuaj');
 				document.getElementById('email').style.borderColor = "red";
 				document.getElementById('email').style.color = "white";
 				document.getElementById('email').style.background = "red";
 				return false;
 			}
-			if(document.getElementById('subjekti').value == ""){
-				alert('Keni harruar subjektin?');
-				document.getElementById('subjekti').style.borderColor = "red";
-				document.getElementById('subjekti').style.color = "white";
-				document.getElementById('subjekti').style.background = "red";
-				return false;
-			}
-			
 		}
 
 function openKyqu(){
-			window.open("Menu-1/kyqu.html","_blank","top=40,bottom=400,left=600,height=600,width=600");
+			window.open("../../Menu-1/kyqu.html","_blank","top=40,bottom=400,left=600,height=600,width=600");
 		}
 		function openRegjistrohu(){
-			window.open("Menu-1/signup.html","_blank","top=0,left=200,height=700,width=1000");
+			window.open("../../Menu-1/signup.html","_blank","top=0,left=200,height=700,width=1000");
 		}
 	
